d6d61bf13519d0c16fe24cc7262fa42b2546d0df
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2024-01-29 23:54:03 +0300

    gh-page and parcel now working

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+const pullsCountLabel = document.getElementById("pullsCountLabel");
+
+const orundumInput = document.getElementById("pullsCountLabel");
+const permitInput = document.getElementById("permitInput");
+const originiumInput = document.getElementById("originiumInput");
+
+function handleInputChange(){
+    console.log("something happened");
+    
+}
