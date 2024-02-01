cbaa6aa6be3ca25f0dfe3145b8c7b450556cbc38
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2024-02-01 10:30:00 +0300

    Updates

---
+++ index.bc9084ba.js
@@ -0,0 +1,9 @@
+const pullsCountLabel = document.getElementById("pullsCountLabel");
+const orundumInput = document.getElementById("pullsCountLabel");
+const permitInput = document.getElementById("permitInput");
+const originiumInput = document.getElementById("originiumInput");
+function handleInputChange() {
+    console.log("something happened");
+}
+
+//# sourceMappingURL=index.bc9084ba.js.map
---
+++ index.f792e1e4.js
@@ -0,0 +1,2 @@
+document.getElementById("pullsCountLabel"),document.getElementById("pullsCountLabel"),document.getElementById("permitInput"),document.getElementById("originiumInput");
+//# sourceMappingURL=index.f792e1e4.js.map
