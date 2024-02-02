0e337c777ab0ac68ba3ce0743b92146dc68332c0
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2024-02-02 10:59:20 +0300

    Nothing is working right now

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
-const pullsCountLabel = document.getElementById("pullsCountLabel");
+const pullsCountLabel = document.getElementById("pullsCountLabel") as HTMLElement;
 
-const orundumInput = document.getElementById("pullsCountLabel");
-const permitInput = document.getElementById("permitInput");
-const originiumInput = document.getElementById("originiumInput");
+const orundumInput = document.getElementById("orundumInput") as HTMLInputElement;
+const permitInput = document.getElementById("permitInput") as HTMLInputElement;
+const originiumInput = document.getElementById("originiumInput") as HTMLInputElement;
 
-function handleInputChange(){
-    console.log("something happened");
-    
+let orundumNum: number = 0;
+let permitNum: number = 0;
+let originiumNum: number = 0;
+
+let pullsNum: number = 0;
+
+function stepUp(target: HTMLInputElement | null, amount: Number) {
+
+}
+
+function stepDown(target: HTMLInputElement | null, amount: Number) {
+
+}
+
+function handleInputChange() {
+  console.log(originiumInput);
+
+  console.log(originiumInput.value);
+
+  orundumNum = parseInt(originiumInput.value);
+  permitNum = parseInt(permitInput.value);
+  originiumNum = parseInt(originiumInput.value);
+
+  console.log(orundumNum);
+  console.log(permitNum);
+  console.log(originiumNum);
+
+  console.log("something happened");
+
+  pullsNum = Math.floor((orundumNum + originiumNum * 180) / 600) + permitNum;
+
+  console.log(pullsNum);
 }
