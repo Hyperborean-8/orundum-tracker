67920dd447cb1232647f8f92ada1828cacbbadca
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2024-02-04 16:17:58 +0300

    Basic things

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 const pullsCountLabel = document.getElementById("pullsCountLabel") as HTMLElement;
 
+let pullsNum: number = 0;
+
+const pullsPercentLabel = document.getElementById("pullsPercentLabel") as HTMLElement;
+const pullsBar = document.getElementById("pullsBar") as HTMLElement;
+const pullsBarProgress = document.getElementById("pullsBarProgress") as HTMLElement;
+
+let pullsPercent = 0;
+
 const orundumInput = document.getElementById("orundumInput") as HTMLInputElement;
 const permitInput = document.getElementById("permitInput") as HTMLInputElement;
 const originiumInput = document.getElementById("originiumInput") as HTMLInputElement;
@@ -8,32 +16,78 @@ let orundumNum: number = 0;
 let permitNum: number = 0;
 let originiumNum: number = 0;
 
-let pullsNum: number = 0;
-
-function stepUp(target: HTMLInputElement | null, amount: Number) {
-
+// These function are temporary
+function stepUpOrundum() {
+  if (orundumInput) {
+    orundumNum += 600;
+    orundumInput.value = orundumNum.toString();
+    handleInputChange();
+  }
 }
 
-function stepDown(target: HTMLInputElement | null, amount: Number) {
+function stepDownOrundum() {
+  if (orundumInput) {
+    orundumNum -= 600;
+    if (orundumNum < 0) {
+      orundumNum = 0;
+    }
+    orundumInput.value = orundumNum.toString();
+    handleInputChange();
+  }
+}
 
+function stepUpPermit() {
+  if (permitInput) {
+    permitNum += 1;
+    permitInput.value = permitNum.toString();
+    handleInputChange();
+  }
+}
+
+function stepDownPermit() {
+  if (permitInput) {
+    permitNum -= 1;
+    if (permitNum < 0) {
+      permitNum = 0;
+    }
+    permitInput.value = permitNum.toString();
+    handleInputChange();
+  }
+}
+
+function stepUpOriginium() {
+  if (originiumInput) {
+    originiumNum += 1;
+    originiumInput.value = originiumNum.toString();
+    handleInputChange();
+  }
+}
+
+function stepDownOriginium() {
+  if (originiumInput) {
+    originiumNum -= 1;
+    if (originiumNum < 0) {
+      originiumNum = 0;
+    }
+    originiumInput.value = originiumNum.toString();
+    handleInputChange();
+  }
 }
 
 function handleInputChange() {
-  console.log(originiumInput);
-
-  console.log(originiumInput.value);
-
-  orundumNum = parseInt(originiumInput.value);
-  permitNum = parseInt(permitInput.value);
-  originiumNum = parseInt(originiumInput.value);
-
-  console.log(orundumNum);
-  console.log(permitNum);
-  console.log(originiumNum);
+  orundumNum = parseInt(orundumInput.value) || 0;
+  permitNum = parseInt(permitInput.value) || 0;
+  originiumNum = parseInt(originiumInput.value) || 0;
 
   console.log("something happened");
 
+  // Pulls count
   pullsNum = Math.floor((orundumNum + originiumNum * 180) / 600) + permitNum;
+  pullsCountLabel.textContent = `You have ${pullsNum} pulls`;
 
-  console.log(pullsNum);
+  // Percents
+  pullsPercent = Math.floor(pullsNum / 300 * 100) || 0;
+  pullsPercentLabel.textContent = `That's ${pullsPercent}% of the required 300 pulls to get a guaranteed limited operator.`
+  pullsBar.setAttribute('aria-valuenow', pullsPercent.toString())
+  pullsBarProgress.style.width = `${pullsPercent}%`;
 }
