089b8b5d3315accebb4e070ec9dd8b32c523e9ff
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2024-02-04 22:14:55 +0300

    Updates

--- index.7edea7e0.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("pullsCountLabel"),document.getElementById("pullsPercentLabel"),document.getElementById("pullsBar"),document.getElementById("pullsBarProgress"),document.getElementById("orundumInput"),document.getElementById("permitInput"),document.getElementById("originiumInput");
-//# sourceMappingURL=index.7edea7e0.js.map
