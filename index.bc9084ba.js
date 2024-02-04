44b817e762dcea1801e73d7c44ae6a17f749f8cf
Hyperborean-8 <85061131+Hyperborean-8@users.noreply.github.com>
2024-02-04 16:18:12 +0300

    Updates

--- index.bc9084ba.js
+++ index.bc9084ba.js
@@ -1,9 +1,74 @@
 const pullsCountLabel = document.getElementById("pullsCountLabel");
-const orundumInput = document.getElementById("pullsCountLabel");
+let pullsNum = 0;
+const pullsPercentLabel = document.getElementById("pullsPercentLabel");
+const pullsBar = document.getElementById("pullsBar");
+const pullsBarProgress = document.getElementById("pullsBarProgress");
+let pullsPercent = 0;
+const orundumInput = document.getElementById("orundumInput");
 const permitInput = document.getElementById("permitInput");
 const originiumInput = document.getElementById("originiumInput");
+let orundumNum = 0;
+let permitNum = 0;
+let originiumNum = 0;
+// These function are temporary
+function stepUpOrundum() {
+    if (orundumInput) {
+        orundumNum += 600;
+        orundumInput.value = orundumNum.toString();
+        handleInputChange();
+    }
+}
+function stepDownOrundum() {
+    if (orundumInput) {
+        orundumNum -= 600;
+        if (orundumNum < 0) orundumNum = 0;
+        orundumInput.value = orundumNum.toString();
+        handleInputChange();
+    }
+}
+function stepUpPermit() {
+    if (permitInput) {
+        permitNum += 1;
+        permitInput.value = permitNum.toString();
+        handleInputChange();
+    }
+}
+function stepDownPermit() {
+    if (permitInput) {
+        permitNum -= 1;
+        if (permitNum < 0) permitNum = 0;
+        permitInput.value = permitNum.toString();
+        handleInputChange();
+    }
+}
+function stepUpOriginium() {
+    if (originiumInput) {
+        originiumNum += 1;
+        originiumInput.value = originiumNum.toString();
+        handleInputChange();
+    }
+}
+function stepDownOriginium() {
+    if (originiumInput) {
+        originiumNum -= 1;
+        if (originiumNum < 0) originiumNum = 0;
+        originiumInput.value = originiumNum.toString();
+        handleInputChange();
+    }
+}
 function handleInputChange() {
+    orundumNum = parseInt(orundumInput.value) || 0;
+    permitNum = parseInt(permitInput.value) || 0;
+    originiumNum = parseInt(originiumInput.value) || 0;
     console.log("something happened");
+    // Pulls count
+    pullsNum = Math.floor((orundumNum + originiumNum * 180) / 600) + permitNum;
+    pullsCountLabel.textContent = `You have ${pullsNum} pulls`;
+    // Percents
+    pullsPercent = Math.floor(pullsNum / 300 * 100) || 0;
+    pullsPercentLabel.textContent = `That's ${pullsPercent}% of the required 300 pulls to get a guaranteed limited operator.`;
+    pullsBar.setAttribute("aria-valuenow", pullsPercent.toString());
+    pullsBarProgress.style.width = `${pullsPercent}%`;
 }
 
 //# sourceMappingURL=index.bc9084ba.js.map
--- index.f792e1e4.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("pullsCountLabel"),document.getElementById("pullsCountLabel"),document.getElementById("permitInput"),document.getElementById("originiumInput");
-//# sourceMappingURL=index.f792e1e4.js.map
